52b51d19321bb9dfb3fa77a5c73009175176ff5a
Ayub Kureshi <ayub.cfhuw22@sinhgad.edu>
2025-10-24 16:58:03 +0530

    Setup React and TailwindCSS

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+
+const App = () => {
+  return (
+    <div className="bg-black p-4">
+      <h1 className="text-6xl text-center text-orange-600 font-extrabold font-sans">Hello, Ayub</h1>
+    </div>
+  )
+}
+
+export default App
